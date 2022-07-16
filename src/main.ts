337bab23a3a9356577122c642eeadae41fd8ce11
balabaev00 <balabaev00@yandex.ru>
2022-07-16 21:14:51 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import {NestFactory} from "@nestjs/core";
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
 import {AppModule} from "./server/app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle("Blog API")
+		.setDescription("The blog api description")
+		.setVersion("1.0")
+		.addTag("blogs")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
